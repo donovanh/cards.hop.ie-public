cb82fd4b699021c9dfc076be96df58a0ba784f3e
Donovan Hutchinson <donovanh@gmail.com>
2025-02-15 10:04:45 +0000

    Updates

--- app/immutable/chunks/getLocaleFromUrl.BA8esNvy.js
+++
@@ -1,1 +0,0 @@
-import{p as t,l as e}from"./index.CRJkzY0H.js";const r=()=>{const a=t.url.pathname.split("/");return e.includes(a[1])?a[1]:"en"};export{r as g};
---
+++ app/immutable/entry/start.Bj24DZNW.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.BCuv7YpD.js";export{t as start};
