f08d492754ee945755e147bab3c9426d0a3a5ba7
Donovan Hutchinson <donovanh@gmail.com>
2025-02-15 18:13:43 +0000

    Updates

---
+++ app/immutable/chunks/cards.BtgQ9tD8.js
@@ -0,0 +1,105 @@
+import{a as p,t as x}from"./disclose-version.C30CovIo.js";import"./legacy.JNgZq3j4.js";import{h as Z,i as Vt,p as ut,S as r,a as vt,aL as $t,a8 as v,a9 as h,a2 as o,ar as u,O as J,a6 as G,aa as H,n as It}from"./runtime.Clv2h664.js";import{d as Ot,s as Gt,g as Bt,a as X,b as Y,h as C}from"./preload-helper.lpx_onmw.js";import{h as qt}from"./html.Dp2mR9o_.js";import{p as Q,t as zt,s as V,a as Nt}from"./index.B_TfYPH-.js";import{p as I}from"./proxy.C7zZTWEY.js";import{p as B,b as tt}from"./props.BoxkulLT.js";import{c as ft,a as P,p as Dt}from"./cardIsOpen.b3mB0ivk.js";import{o as et,a as Mt}from"./index-client.B8Jf1U8c.js";import{m as kt}from"./metadata.DylG7R3E.js";import{i as Et}from"./lifecycle.Bb7xKLtc.js";function Lt(t,e,n,s,a){var y;Z&&Vt();var F=(y=e.$$slots)==null?void 0:y[n],d=!1;F===!0&&(F=e.children,d=!0),F===void 0||F(t,d?()=>s:s)}function Ht(t,e,n){var s=t.__className,a=Xt(e);Z&&t.className===a?t.__className=a:(s!==a||Z&&t.className!==a)&&(e==null&&!n?t.removeAttribute("class"):t.className=a,t.__className=a)}function Xt(t,e){return(t??"")+""}function Yt(t,e,n){{if(t.classList.contains(e))return;t.classList.add(e)}}var Pt=x("<div><!></div>");function At(t,e){ut(e,!1);let n=B(e,"root",8,null),s=B(e,"rootMargin",8,"200px"),a=B(e,"threshold",8,.01),F=B(e,"once",8,!1),d=B(e,"intersecting",12,!1),y=$t(null),f;et(()=>{f=new IntersectionObserver(([_])=>{d(_.isIntersecting),d()&&F()&&f.disconnect()},{root:n(),rootMargin:s(),threshold:a()}),r(y)&&f.observe(r(y))}),Mt(()=>{f==null||f.disconnect()}),Et();var g=Pt(),K=v(g);Lt(K,e,"default",{get intersecting(){return d()}}),h(g),tt(g,_=>o(y,_),()=>r(y)),p(t,g),vt()}var Rt=x('<button><div class="back svelte-e4ye0q"><div class="content svelte-e4ye0q"><p class="intro svelte-e4ye0q"> </p> <p class="main-content svelte-e4ye0q"> </p> <p class="sign-off svelte-e4ye0q"> </p></div></div> <div class="front svelte-e4ye0q"><div class="hardcoded-front-content svelte-e4ye0q"><!></div> <div class="content svelte-e4ye0q"><p class="svelte-e4ye0q"> </p></div></div> <div class="lining svelte-e4ye0q"></div></button>'),Ut=x("<div><!></div>");function c(t,e){ut(e,!0);const n=Gt(),s=()=>X(P,"$cardIsOpen",n),a=()=>X(ft,"$cardText",n),F=()=>X(Nt,"$i18n",n),d=()=>X(zt,"$t",n),{decompressFromEncodedURIComponent:y}=Dt;let f="closed",g="open";const K=Q.url.pathname.includes("edit"),_=Q.url.pathname.includes("view"),ht=!K&&!_,A=K?"edit":_?"view":"preview",nt=e.cardClass.split(" ")[0],{isWide:mt,defaultText:at,hardcodedFrontText:rt}=kt[nt];let q=u(!1),S=u(I(K&&s()?g:f));J(()=>{K?s()?o(S,g):o(S,f):r(q)?o(S,g):o(S,f)});let m=u(I(K||ht?a():at));J(()=>{o(m,I(K?a():at))});function pt(){s()?(P.updateCardIsOpen(!1),o(q,!1)):(o(N,0),o(D,0),P.updateCardIsOpen(!0),o(q,!0))}function xt(){A==="preview"&&(o(S,f),P.updateCardIsOpen(!1),o(q,!1))}let z=u(null),R=u(null),N=u(0),D=u(0),st=u(0),ot=u(0),yt=I(_?.5:0);function it(i){if(r(R))if(r(S)===f&&A==="view"){const{innerWidth:T,innerHeight:l}=window,b=i.clientX/T*2-1,w=i.clientY/l*2-1;o(N,b*10),o(D,w*-10),ct(b,w)}else o(N,0),o(D,0),ct(-400,-400)}function ct(i,T){if(!r(z))return;const l=r(z).getBoundingClientRect();o(st,1-i*(l.right-l.left)),o(ot,1-T*(l.bottom-l.top))}function Tt(){const i=Q.url.searchParams.get("data")||"",T=y(i),l=decodeURIComponent;if(!T)return;const[b,w,$,U]=T.split("|").map(l);return{frontText:b==="null"?null:b,innerText1:w,innerText2:$,innerText3:U}}let bt=u(!1);et(()=>o(bt,!0)),et(()=>{if(_){const i=Tt();i&&(o(m,I(i)),ft.updateText(r(m)))}return window.addEventListener("mousemove",it),()=>{window.removeEventListener("mousemove",it)}});const Ft=i=>i?i.split("").map(T=>`<span>${T}</span>`).join(""):"";let lt=u("");J(()=>{r(M)&&o(lt,`/images/cards/${nt}.webp`)});let M=u(!1);var k=Ut(),gt=v(k);At(gt,{once:!0,get intersecting(){return r(M)},set intersecting(i){o(M,I(i))},children:(i,T)=>{var l=Rt();l.__click=pt;var b=v(l),w=v(b),$=v(w),U=v($,!0);G(()=>Y(U,r(m).innerText1?d()(r(m).innerText1):"")),h($);var O=H($,2),Kt=v(O,!0);G(()=>Y(Kt,r(m).innerText2?d()(r(m).innerText2):"")),h(O);var W=H(O,2),_t=v(W,!0);G(()=>Y(_t,r(m).innerText3?d()(r(m).innerText3):"")),h(W),h(w),h(b);var E=H(b,2),L=v(E),Ct=v(L);qt(Ct,()=>rt&&Ft(d()(rt))),h(L);var dt=H(L,2),j=v(dt),St=v(j,!0);G(()=>Y(St,r(m).frontText?d()(r(m).frontText):"")),h(j),h(dt),h(E),It(2),h(l),tt(l,wt=>o(R,wt),()=>r(R)),G(()=>{V(l,"style",`transform: rotateY(${r(N)??""}deg) rotateX(${r(D)??""}deg); opacity: ${(r(M)?1:0)??""}`),Ht(l,`card ${r(S)??""} ${F().language??""} ${e.cardClass??""} ${A??""} ${(mt?"wide":"tall")??""} svelte-e4ye0q`),V($,"style",`font-family: ${e.fonts.innerText1Font??""}`),V(O,"style",`font-family: ${e.fonts.innerText2Font??""}`),V(W,"style",`font-family: ${e.fonts.innerText3Font??""}`),V(E,"style",`--sheenX: ${r(st)??""}px; --sheenY: ${r(ot)??""}px; --sheenOpacity: ${yt??""}; background-image: url(${r(lt)??""});`),Yt(E,"loaded"),V(L,"style",`font-family: ${e.fonts.frontTextFont??""}`),V(j,"style",`font-family: ${e.fonts.frontTextFont??""}`)}),Bt("mouseleave",l,xt),p(i,l)},$$slots:{default:!0}}),h(k),tt(k,i=>o(z,i),()=>r(z)),p(t,k),vt()}Ot(["click"]);function Wt(t){c(t,{fonts:{frontTextFont:'"Great Vibes", cursive',innerText1Font:'"Kaushan Script", cursive',innerText2Font:'"Great Vibes", cursive',innerText3Font:'"Kaushan Script", cursive'},cardClass:"valentines009"})}function jt(t){c(t,{fonts:{frontTextFont:"Kalam, cursive",innerText1Font:"Kalam, cursive",innerText2Font:'"Kaushan Script", cursive',innerText3Font:"Kalam, cursive"},cardClass:"valentines008"})}function Jt(t){c(t,{fonts:{frontTextFont:"Kalam, cursive",innerText1Font:"Kalam, cursive",innerText2Font:'"Kaushan Script", cursive',innerText3Font:"Kalam, cursive"},cardClass:"valentines007"})}function Qt(t){c(t,{fonts:{frontTextFont:'"Great Vibes", cursive',innerText1Font:'"Kaushan Script", cursive',innerText2Font:'"Great Vibes", cursive',innerText3Font:'"Kaushan Script", cursive'},cardClass:"valentines006"})}var Zt=x(`<style>.valentines005 .hardcoded-front-content {
+      color: #111;
+      font-weight: bold;
+      line-height: 1.25em;
+      top: 0.4em;
+    }</style>`);function te(t){const e={frontTextFont:"Kalam, cursive",innerText1Font:"Kalam, cursive",innerText2Font:'"Kaushan Script", cursive',innerText3Font:"Kalam, cursive"},n="valentines005";C(s=>{var a=Zt();p(s,a)}),c(t,{fonts:e,cardClass:n})}var ee=x(`<style>.valentines004 .hardcoded-front-content {
+      color: #f11;
+      font-size: 7em;
+      line-height: 1.25em;
+      top: 0.4em;
+      right: 0.25em;
+      left: 2em;
+    }</style>`);function ne(t){const e={frontTextFont:'"Great Vibes", cursive',innerText1Font:'"Kaushan Script", cursive',innerText2Font:'"Great Vibes", cursive',innerText3Font:'"Kaushan Script", cursive'},n="valentines004";C(s=>{var a=ee();p(s,a)}),c(t,{fonts:e,cardClass:n})}function ae(t){c(t,{fonts:{frontTextFont:"Kalam, cursive",innerText1Font:"Kalam, cursive",innerText2Font:'"Kaushan Script", cursive',innerText3Font:"Kalam, cursive"},cardClass:"valentines003"})}function re(t){c(t,{fonts:{frontTextFont:'"Great Vibes", cursive',innerText1Font:'"Kaushan Script", cursive',innerText2Font:'"Great Vibes", cursive',innerText3Font:'"Kaushan Script", cursive'},cardClass:"valentines002"})}function se(t){c(t,{fonts:{frontTextFont:'"Great Vibes", cursive',innerText1Font:'"Kaushan Script", cursive',innerText2Font:'"Great Vibes", cursive',innerText3Font:'"Kaushan Script", cursive'},cardClass:"valentines001 fancy-wide"})}var oe=x(`<style>.birthday009 .hardcoded-front-content {
+      font-size: 8em;
+      text-shadow: 0.05em 0.05em 0.02em rgba(0, 0, 0, 0.15);
+      top: 0.3em;
+      bottom: auto;
+      line-height: 1.4em;
+    }
+
+    .birthday009.de .hardcoded-front-content,
+    .birthday009.fr .hardcoded-front-content {
+      font-size: 6em;
+    }</style>`);function ie(t){const e={frontTextFont:'"Kaushan Script", cursive',innerText1Font:"Kalam, cursive",innerText2Font:'"Kaushan Script", cursive',innerText3Font:"Kalam, cursive"},n="birthday009";C(s=>{var a=oe();p(s,a)}),c(t,{fonts:e,cardClass:n})}var ce=x(`<style>.birthday008 .front .content {
+      top: auto;
+      text-align: center;
+      left: 0;
+      right: 0;
+      top: 60%;
+      margin: -50% 0 0 -5em;
+    }
+
+    .birthday008 .front .content p {
+      color: rgba(255, 255, 255, 0.85);
+      text-shadow: 0.02em 0.02em 0.02em rgba(0, 0, 0, 0.45);
+      font-size: 32em;
+      letter-spacing: -0.1em;
+      line-height: 1em;
+    }</style>`);function le(t){const e={frontTextFont:"Kalam, cursive",innerText1Font:"Kalam, cursive",innerText2Font:'"Kaushan Script", cursive',innerText3Font:"Kalam, cursive"},n="birthday008";C(s=>{var a=ce();p(s,a)}),c(t,{fonts:e,cardClass:n})}var de=x(`<style>.birthday007 .front .content {
+      top: 6em;
+    }
+
+    .birthday007.fr .front .content {
+      top: 10em;
+    }
+
+    .birthday007.de .front .content {
+      top: 10em;
+    }
+
+    .birthday007 .front .content p {
+      color: #fff;
+      text-shadow: 0.05em 0.05em 0.02em rgba(0, 0, 0, 0.75);
+    }
+
+    :global {
+      .birthday007 .hardcoded-front-content span:nth-child(3n + 1) {
+        color: rgb(18, 160, 255);
+      }
+
+      .birthday007 .hardcoded-front-content span:nth-child(3n + 2) {
+        color: white;
+      }
+
+      .birthday007 .hardcoded-front-content span:nth-child(3n + 3) {
+        color: #f73500;
+      }
+    }
+
+    .birthday007 .hardcoded-front-content {
+      font-size: 5em;
+      text-shadow: 0.05em 0.05em 0.02em rgba(0, 0, 0, 0.75);
+      top: 0.5em;
+      bottom: auto;
+    }
+
+    .birthday007.de .hardcoded-front-content,
+    .birthday007.fr .hardcoded-front-content {
+      line-height: 1.25;
+      top: 0.25em;
+    }</style>`);function fe(t){const e={frontTextFont:"Kalam, cursive",innerText1Font:"Kalam, cursive",innerText2Font:'"Kaushan Script", cursive',innerText3Font:"Kalam, cursive"},n="birthday007";C(s=>{var a=de();p(s,a)}),c(t,{fonts:e,cardClass:n})}var ue=x(`<style>.birthday006 .hardcoded-front-content {
+      font-size: 8em;
+      text-shadow: 0.05em 0.05em 0.02em rgba(0, 0, 0, 0.15);
+      top: 0.3em;
+      bottom: auto;
+      line-height: 1.4em;
+    }
+
+    .birthday006.de .hardcoded-front-content,
+    .birthday006.fr .hardcoded-front-content {
+      font-size: 6em;
+    }</style>`);function ve(t){const e={frontTextFont:'"Kaushan Script", cursive',innerText1Font:"Kalam, cursive",innerText2Font:'"Kaushan Script", cursive',innerText3Font:"Kalam, cursive"},n="birthday006";C(s=>{var a=ue();p(s,a)}),c(t,{fonts:e,cardClass:n})}var he=x(`<style>.birthday005 .hardcoded-front-content {
+      top: 0.5em;
+      bottom: auto;
+      font-size: 4.75em;
+    }</style>`);function me(t){const e={frontTextFont:'"Kaushan Script", cursive',innerText1Font:"Kalam, cursive",innerText2Font:'"Kaushan Script", cursive',innerText3Font:"Kalam, cursive"},n="birthday005";C(s=>{var a=he();p(s,a)}),c(t,{fonts:e,cardClass:n})}function pe(t){c(t,{fonts:{frontTextFont:"Kalam, cursive",innerText1Font:"Kalam, cursive",innerText2Font:'"Kaushan Script", cursive',innerText3Font:"Kalam, cursive"},cardClass:"birthday004"})}function xe(t){c(t,{fonts:{frontTextFont:'"Kaushan Script", cursive',innerText1Font:'"Kaushan Script", cursive',innerText2Font:'"Great Vibes", cursive',innerText3Font:'"Kaushan Script", cursive'},cardClass:"happybirthday003"})}function ye(t){c(t,{fonts:{frontTextFont:"Kalam, cursive",innerText1Font:"Kalam, cursive",innerText2Font:'"Kaushan Script", cursive',innerText3Font:"Kalam, cursive"},cardClass:"happybirthday001"})}var Te=x(`<style>.happybirthday002 .front .content {
+      top: 0;
+    }
+
+    .happybirthday002 .front .content p {
+      line-height: 1em;
+      font-size: 20em;
+      color: rgb(57, 0, 57);
+      text-shadow: 0.05em 0.05em 0.02em rgba(0, 0, 0, 0.15);
+    }</style>`);function be(t){const e={frontTextFont:'"Great Vibes", cursive',innerText1Font:'"Kaushan Script", cursive',innerText2Font:'"Great Vibes", cursive',innerText3Font:'"Kaushan Script", cursive'},n="happybirthday002";C(s=>{var a=Te();p(s,a)}),c(t,{fonts:e,cardClass:n})}const Be={valentines009:{component:Wt},valentines008:{component:jt},valentines007:{component:Jt},valentines006:{component:Qt},valentines005:{component:te},valentines004:{component:ne},valentines003:{component:ae},valentines002:{component:re},valentines001:{component:se},birthday009:{component:ie},birthday008:{component:le},birthday007:{component:fe},birthday006:{component:ve},birthday005:{component:me},birthday004:{component:pe},happybirthday003:{component:xe},happybirthday001:{component:ye},happybirthday002:{component:be}};export{At as I,Be as c};
--- app/immutable/entry/start.BIRxjMMv.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.yb3ScwLM.js";export{t as start};
